ebc40aa4b22fdef1476d04cfcf59e01155f8def2
adityanaag3 <atopalli@salesforce.com>
2020-09-09 14:54:27 +0530

    Handling PWA updates

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -3,5 +3,12 @@
 const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
-    plugins: [new GenerateSW({ swDest: 'sw.js' })]
+    plugins: [
+        new GenerateSW({
+            swDest: 'sw.js',
+            cacheId: 'snakelwc',
+            skipWaiting: true,
+            clientsClaim: true
+        })
+    ]
 };
