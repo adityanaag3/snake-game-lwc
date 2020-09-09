2e6e101dc9915c1153aa5e77256183ef66dbe669
adityanaag3 <atopalli@salesforce.com>
2020-09-09 13:40:06 +0530

    Enabling PWA

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -2,5 +2,9 @@
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
     buildDir: './docs',
-    resources: [{ from: 'src/resources/', to: 'docs/resources/' }]
+    resources: [
+        { from: 'src/resources/', to: 'docs/resources/' },
+        { from: 'src/index.html', to: 'docs/' },
+        { from: 'src/manifest.json', to: 'docs/' }
+    ]
 };
